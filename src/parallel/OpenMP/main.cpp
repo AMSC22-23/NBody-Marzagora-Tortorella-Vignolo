273c822d088ee66f08b50fb2a65f8d9d8031ed63
bicemar <bicemarzagora@gmail.com>
2023-12-01 10:54:08 +0000

    Fixed use of template

--- src/parallel/OpenMP/main.cpp
+++ src/parallel/OpenMP/main.cpp
@@ -10,8 +10,9 @@
 #include <omp.h>
 
 // Function that randomly generates particles:
-std::vector<Particle> generateRandomParticlesParallel(int N = 10, int posBoundary = 100, int minMass = 1, int maxMass = 99, int maxVx = 100, int maxVy = 100, int minRadius = 0, int maxRadius = 15, bool type = false) {
-    std::vector<Particle> particles;
+template<size_t Dimension>
+std::vector<Particle<Dimension>> generateRandomParticlesParallel(int N = 10, int posBoundary = 100, int minMass = 1, int maxMass = 99, int maxVx = 100, int maxVy = 100, int minRadius = 0, int maxRadius = 15, bool type = false) {
+    std::vector<Particle<Dimension>> particles;
 
     // Initialize random seed
     unsigned int seed;
@@ -83,10 +84,12 @@ std::vector<Particle> generateRandomParticlesParallel(int N = 10, int posBoundar
 }
 
 
-std::vector<Particle> generateRandomParticlesSerial(int N, int posBoundary = 100, int minProperty = 1, int maxProperty = 99, int maxVx = 50, int maxVy = 50, int minRadius = 0, int maxRadius = 15, bool type = false) {
-    std::vector<Particle> particles;
 
-    // Initialize random seed
+template<size_t Dimension>
+std::vector<Particle<Dimension>> generateRandomParticlesSerial(int N, int posBoundary = 100, int minProperty = 1, int maxProperty = 99, int maxVx = 50, int maxVy = 50, int minRadius = 0, int maxRadius = 15, bool type = false) {
+    std::vector<Particle<Dimension>> particles;
+
+    // Initialize random seedgit pull
     srand(time(0));
 
     bool uniquePosition = false;
@@ -133,38 +136,29 @@ std::vector<Particle> generateRandomParticlesSerial(int N, int posBoundary = 100
 }
 
 
-
 int main() {
+    // Define of variables
+    const double delta_t = 0.01; // in seconds
+    const double dim = 500; // Dimension of the simulation area
 
+    // number of iteration
+    int it = 1000;
     
+    //2D or 3D
+    const int d = 2;
 
-    // Definition of variables
-    const double delta_t = 0.01; // [sec]
-    int dim = 50000; // Dimension of the simulation area
-    int it = 1000; // Number of iteration
-    int n = 5000; // Number of particles
-    double softening = 0.7; // Softening parameter
-    std::vector<Particle> particles;  // Create a vector of particles
-    CustomForce f;
+    // Create a vector of particles
+    std::vector<Particle<d>> particles;
+    Force<d>* f = new CustomForce<d>();
     std::string fileName = "../graphics/coordinates.txt";
     std::ofstream file(fileName); // Open file
     
+    //generate 100 particles
+    int n = 50;
+    particles = generateRandomParticlesParallel<d>(n, dim, 1, 99, 50, 50, 1, 10, false);
 
-    clock_t start = clock();
-    generateRandomParticlesParallel(n, dim, 1, 99, 50, 50, 1, 10, false);
-    clock_t end = clock();
-    double duration = double(end - start) / CLOCKS_PER_SEC;
-    std::cout << "Time taken by parallel function: " << duration << " seconds" << std::endl;
+    double softening = 0.7; // Softening parameter
 
-    start = clock();
-    generateRandomParticlesSerial(n, dim, 1, 99, 50, 50, 1, 10, false);
-    end = clock();
-    duration = double(end - start) / CLOCKS_PER_SEC;
-    std::cout << "Time taken by serial function: " << duration << " seconds" << std::endl;
-    //call the generateRandomParticles function to generate the particles and put minRadius = 1
-    particles  = generateRandomParticles(n, dim, 1, 99, 50, 50, 1, 10, false);
-
-/*
     // Print the initial state of the particles
     std::cout << "Initial state:\n";
     if (file.is_open()) {
@@ -176,11 +170,21 @@ int main() {
         file << dim << std::endl;
 
         // Write on file the radius of the particles and the initial state
-        for (const Particle& p : particles) 
+        for (const Particle<d> & p : particles) 
             file << p.getRadius() << std::endl;
-        for (const Particle& p : particles) {
-            file << p.getId() << "," << p.getPos()[0] << "," <<  p.getPos()[1] << std::endl;
-    }
+        for (const Particle<d> &p : particles) {
+            //file << p.getId() << "," << p.getPos()[0] << "," <<  p.getPos()[1] << std::endl;
+            file << p.getId() << ",";
+
+            const auto& pos = p.getPos();
+            for (size_t i = 0; i < d; ++i) {
+                file << pos[i];
+                if (i < d - 1) file << ",";
+            }
+
+            file << std::endl;
+
+        }
     }else {
         std::cout << "Unable to open file";
     }
@@ -188,35 +192,49 @@ int main() {
     // Start of the simulation
     for (int z = 0; z < it; ++z){
         for (int i = 0; i < particles.size(); ++i) {
-            Particle &q = particles[i];
+            Particle<d> &q = particles[i];
 
             // Check if the particle hits the bounday
-            if(q.getPos()[0] + q.getRadius() > dim || 
-                q.getPos()[0] - q.getRadius() < -dim ||
-                q.getPos()[1] + q.getRadius()> dim || 
-                q.getPos()[1] - q.getRadius()< -dim){
-                q.manage_collision(q, dim);
+
+            if(q.hitsBoundary(dim)){
+                q.manageCollision(q, dim);
             }
             for (int j = i + 1; j < particles.size(); j++) {
-                Particle &k = particles[j];
+                Particle<d> &k = particles[j];
 
                 // check collisions between particles
-                if(q.square_distance(k) < (((q.getRadius() + k.getRadius())*(q.getRadius() + k.getRadius())))*softening){
+                if(q.squareDistance(k) < (((q.getRadius() + k.getRadius())*(q.getRadius() + k.getRadius())))*softening){
 
                     // Call the collision method
-                    q.manage_collision(k, 0.0);
+                    q.manageCollision(k, 0.0);
                 }
-                // Add force
-                q.addForce(k, f);
+
+                std::array<double, d> force_qk;
+                //force_qk = f.calculateForce(k,q);
+                q.addForce(k, *f);
             }
-            z==it-1? q.update(delta_t):q.update_and_reset(delta_t);
+            z==it-1? q.update(delta_t):q.updateAndReset(delta_t);
         }
         
         // Write on file the updates after delta_t
         for (int i = 0; i < particles.size(); i++) {
-            Particle &q = particles[i];
+            Particle<d> &q = particles[i];
+            // Write on file the updates after delta_t
             if (file.is_open()) {
-                file << q.getId() << "," << q.getPos()[0] << "," <<  q.getPos()[1] << std::endl;
+
+                //file << q.getId() << "," << q.getPos()[0] << "," <<  q.getPos()[1] << std::endl;
+                file << q.getId() << ",";
+
+                const auto& pos = q.getPos();
+                for (size_t i = 0; i < d; ++i) {
+                    file << pos[i];
+                    if (i < d - 1) {
+                        file << ",";
+                    }
+                }
+
+                file << std::endl;
+
             } else {
                 std::cout << "Unable to open file";
             }
@@ -226,12 +244,13 @@ int main() {
     // Print the final state of the particles
     std::cout << "--------------------------------------------\n";
     std::cout << "Final state:\n";
-    for (const Particle& p : particles) {
-        p.print_states();
+    for (const Particle<d> &p : particles) {
+        p.printStates();
 
-        std::cout << "Distance from origin: " << sqrt(pow(p.getPos()[0],2) + pow(p.getPos()[1],2)) << "\n";
+        double power = 0.0;
+        for (size_t i = 0; i < d; ++i) power = power + (p.getPos()[i] * p.getPos()[i]);
+        std::cout << "Distance from origin: " << sqrt(power) << "\n";
     }
     file.close();
-*/
     return 0;
 }
